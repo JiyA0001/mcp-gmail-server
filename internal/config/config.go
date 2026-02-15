bbd2c4eeb817df0b2e186e019a30bd71f6424738
Jiya <jiya2aggarwal@gmail.com>
2026-02-15 21:29:45 +0530

    feat: add configuration loading for Google OAuth credentials.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -14,9 +13,8 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("RAILWAY_ENVIRONMENT") == "" {
+		_ = godotenv.Load()
 	}
 
 	return &Config{
