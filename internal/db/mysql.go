f1723f90b6814ba605674a7360e76b5921dc6f5f
Jiya <jiya2aggarwal@gmail.com>
2026-02-16 10:04:57 +0530

    feat: Add initial MySQL database package with connection setup.

--- internal/db/mysql.go
+++ internal/db/mysql.go
@@ -11,15 +11,15 @@ var DB *sql.DB
 
 func Init() error {
 	dsn := os.Getenv("MYSQL_DSN")
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		return err
-	}
+	// db, err := sql.Open("mysql", dsn)
+	// if err != nil {
+	// 	return err
+	// }
 
-	if err := db.Ping(); err != nil {
-		return err
-	}
+	// if err := db.Ping(); err != nil {
+	// 	return err
+	// }
 
-	DB = db
+	// DB = db
 	return nil
 }
