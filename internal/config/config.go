e71969346b08d6c36ba4428cf8d950703d8ff66a
Jiya <jiya2aggarwal@gmail.com>
2026-02-16 10:38:58 +0530

    feat: add initial configuration loading for Google OAuth2 credentials.

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,12 +4,16 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 type Config struct {
 	ClientID     string
 	ClientSecret string
 	RedirectURL  string
+	Scopes       []string
+	Endpoint     oauth2.Endpoint
 }
 
 func LoadConfig() *Config {
@@ -21,5 +25,7 @@ func LoadConfig() *Config {
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
 		RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
+		Scopes:       []string{"https://www.googleapis.com/auth/gmail.readonly"},
+		Endpoint:     google.Endpoint,
 	}
 }
